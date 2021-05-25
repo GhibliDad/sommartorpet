6f97b41b2dc5170414d0b9be0b5c08015b07d31a
Rui Ribeiro <ribeiro.r.rui@gmail.com>
2021-05-25 15:22:20 +0200

    finished destroy method and added some basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'summer_houses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :summer_houses, except: [:edit, :update]
+  resources :summer_houses, except: [:index, :edit, :update]
   # route for user show page
   get "users/:id", to: "users#show"
 end
