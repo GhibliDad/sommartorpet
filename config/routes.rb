ae56fb3421322b12a246c685bb1f99da3468a767
Daniela Reijs <danielareijs@gmail.com>
2021-05-25 15:58:39 +0200

    correct routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'summer_houses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :summer_houses, except: [:index, :edit, :update]
-  resources :bookings, only: [:show, :new, :create, :destroy]
+  resources :summer_houses, except: [:index, :edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:show, :destroy]
   # route for user show page
   get "users/:id", to: "users#show"
 end
