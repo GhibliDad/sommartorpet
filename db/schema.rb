491f1a959c0caf9bfebb1f0174214a9cde749945
Towa Shimizu <towashimizu@gmail.com>
2021-05-27 14:35:03 +0200

    saving after updateing colums in summer houses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_102958) do
+ActiveRecord::Schema.define(version: 2021_05_27_123140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_102958) do
     t.string "title"
     t.text "description"
     t.float "price"
-    t.string "location"
+    t.string "city"
     t.integer "capacity"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_102958) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
